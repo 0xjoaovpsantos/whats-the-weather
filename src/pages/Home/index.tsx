623214e847456ccc8842aa5eed4455f8f2bc381f
Joao Victor Santos <joaovictorsantoscpv@gmail.com>
2020-08-25 15:48:20 -0300

    chore: home page structure initial

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container, Text } from './styles';
+
+const Home: React.FC = () => (
+  <Container>
+    <Text>Home Page</Text>
+  </Container>
+);
+
+export default Home;
