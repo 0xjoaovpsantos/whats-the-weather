9cd06efcce08aec2ba80cf66f6d6da73096bd349
Joao Victor Santos <joaovictorsantoscpv@gmail.com>
2020-08-25 15:51:04 -0300

    chore: create colors file

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,4 +1,10 @@
 import styled from 'styled-components/native';
+import AppColors from '../../utils/AppColors';
 
-export const Container = styled.View``;
-export const Text = styled.Text``;
+export const Container = styled.View`
+  flex: 1;
+  background-color: #ec8d0f;
+`;
+export const Text = styled.Text`
+  color: ${AppColors.fontColor};
+`;
